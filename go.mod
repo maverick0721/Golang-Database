3a47783b8d05ca4df198459e2493661edae43905
SHIVANG GUPTA <shivang0721gupta@gmail.com>
2024-11-17 23:08:20 +0530

    Write operation created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maverick0721/Golang-Database
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Address struct {
+	City    string
+	State   string
+	Country string
+	Pincode json.Number
+}
+
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
+
+func main() {
+	dir := "./"
+
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	friends = []User{
+		{"Yash", "21", "0987654321", "Google", Address{"Bangalore", "Karnataka", "India", "560001"}},
+		{"Siddharth", "22", "0987654444", "Microsoft", Address{"Bangalore", "Karnataka", "India", "560001"}},
+		{"Manav", "20", "0987653333", "Adobe", Address{"Bangalore", "Karnataka", "India", "560001"}},
+		{"Shailendra", "23", "0987652222", "Netflix", Address{"Bangalore", "Karnataka", "India", "560001"}},
+		{"Aman", "21", "0987651111", "Apple", Address{"Gwalior", "Madhya Pradesh", "India", "474011"}},
+		{"Priyanshu", "21", "0987650000", "Amazon", Address{"Bangalore", "Karnataka", "India", "560001"}},
+	}
+
+	for _, value := range friends {
+		db.Write("users", value.Name, User)
+	}
+}
